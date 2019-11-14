d4309655e3cc22320e77678af38b98686813bcbb
Sushant-ABdigital <borsaniasushant@gmail.com>
2019-11-14 16:48:54 +0000

    practise querying database

---
+++ bootcamp_app/students.js
@@ -0,0 +1,53 @@
+const { Pool } = require("pg");
+
+const pool = new Pool({
+  user: "vagrant",
+  password: "123",
+  host: "localhost",
+  database: "bootcampx"
+});
+
+// pool
+//   .query(
+//     `
+// SELECT students.id, students.name, cohorts.name
+// FROM students
+// JOIN cohorts ON cohorts.id = students.cohort_id
+// LIMIT 5;
+// `
+//   )
+//   .then(res => {
+//     console.log(res);
+//   })
+//   .catch(err => console.error("query error", err.stack));
+
+// pool
+//   .query(
+//     `
+//   SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+//   FROM students
+//   JOIN cohorts ON cohorts.id = cohort_id
+//   LIMIT 5;
+//   `
+//   )
+//   .then(res => {
+//     res.rows.forEach(user => {
+//       console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
+//     });
+//   });
+
+pool
+  .query(
+    `
+SELECT students.id, students.name, cohorts.name as cohort_name
+FROM students
+JOIN cohorts ON cohorts.id = students.cohort_id
+WHERE cohorts.name LIKE '${process.argv[2]}%'
+LIMIT ${process.argv[3]};
+ `
+  )
+  .then(res => {
+    res.rows.forEach(user => {
+      console.log(`${user.name} has an id of ${user.id} and was in the ${user.cohort_name} cohort`);
+    });
+  }).catch(err => console.error('query error', err.stack));
