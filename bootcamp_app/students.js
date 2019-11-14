af1a9114d64bdeed974b692c4270130a7183ad20
Sushant-ABdigital <borsaniasushant@gmail.com>
2019-11-14 18:01:54 +0000

    Update code to prevent sql injection

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -36,18 +36,38 @@ const pool = new Pool({
 //     });
 //   });
 
-pool
-  .query(
-    `
-SELECT students.id, students.name, cohorts.name as cohort_name
-FROM students
-JOIN cohorts ON cohorts.id = students.cohort_id
-WHERE cohorts.name LIKE '${process.argv[2]}%'
-LIMIT ${process.argv[3]};
- `
-  )
-  .then(res => {
-    res.rows.forEach(user => {
-      console.log(`${user.name} has an id of ${user.id} and was in the ${user.cohort_name} cohort`);
-    });
+// pool
+//   .query(
+//     `
+// SELECT students.id, students.name, cohorts.name as cohort_name
+// FROM students
+// JOIN cohorts ON cohorts.id = students.cohort_id
+// WHERE cohorts.name LIKE '${process.argv[2]}%'
+// LIMIT ${process.argv[3]};
+//  `
+//   )
+// .then(res => {
+//   res.rows.forEach(user => {
+//     console.log(`${user.name} has an id of ${user.id} and was in the ${user.cohort_name} cohort`);
+//   });
+// });
+
+//Changing the query to prevent sql injection
+const queryString = `
+  SELECT students.id, students.name, cohorts.name as cohort_name
+  FROM students
+  JOIN cohorts ON cohorts.id = students.cohort_id
+  WHERE cohorts.name LIKE $1
+  LIMIT $2;
+`;
+
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+//Storing malicious values
+const values = [`%${cohortName}%`, limit];
+
+pool.query(queryString, values).then(res => {
+  res.rows.forEach(user => {
+    console.log(`${user.name} has an id of ${user.id} and was in the ${user.cohort_name} cohort`);
   });
+});
